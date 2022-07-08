610d1ed2eac37ec1a5b7467f9b0fa8cf14a4932c
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-07-08 17:26:04 -0300

    Atualizando módulo 2 Dominando IDEs Java

--- Módulo 02/IntelliJ/primeiroProjetoIntelliJ/src/br/com/dio/santander/PrimeiroProjetoIntelliJ.java
+++ Módulo 02/IntelliJ/primeiroProjetoIntelliJ/src/br/com/dio/santander/PrimeiroProjetoIntelliJ.java
@@ -6,13 +6,41 @@ public class PrimeiroProjetoIntelliJ {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro = new Livro("O Problema do Azul", 389);
 
 
        /*int a = 5;
        int b = 3;
 
         System.out.println("Hello World!" + (a+b));*/
+    }
+
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
     }
 }
