ac3b25bcbfd50ae77c17347892c3da79654ad5db
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-07-08 17:17:15 -0300

    Atualizando módulo 2 Dominando IDEs Java

---
+++ Módulo 02/IntelliJ/primeiroProjetoIntelliJ/src/br/com/dio/santander/PrimeiroProjetoIntelliJ.java
@@ -0,0 +1,18 @@
+package br.com.dio.santander;
+
+import br.com.dio.santander.model.Gato;
+
+public class PrimeiroProjetoIntelliJ {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+       /*int a = 5;
+       int b = 3;
+
+        System.out.println("Hello World!" + (a+b));*/
+    }
+}
