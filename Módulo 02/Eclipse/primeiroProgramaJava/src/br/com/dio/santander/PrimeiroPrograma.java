bd34c24cea4b4630905d0ee50a82aeda1d6fe959
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-07-08 16:15:08 -0300

    Atualizando módulo 2 Dominando IDEs Java

---
+++ Módulo 02/Eclipse/primeiroProgramaJava/src/br/com/dio/santander/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio.santander;
+
+
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+			/*System.out.println("Hello World!/ Olá mundo!");
+			int a = 2;
+			int b = 5;
+			System.out.println("A soma é: " + a+b);
+		*/
+		//	Gato gato = new Gato();
+			Livros livros = new Livros();
+			
+			System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	private String nome = "luisa";
+	private String npag = "25";
+	
+	
+}
