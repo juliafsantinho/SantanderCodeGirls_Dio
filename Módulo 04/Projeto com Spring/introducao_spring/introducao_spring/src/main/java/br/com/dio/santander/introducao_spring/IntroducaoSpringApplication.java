dbd80e1c653da032da111197bf93dba52dcf2de4
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-07-22 16:14:03 -0300

    Atualização final

---
+++ Módulo 04/Projeto com Spring/introducao_spring/introducao_spring/src/main/java/br/com/dio/santander/introducao_spring/IntroducaoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.santander.introducao_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoSpringApplication.class, args);
+	}
+
+}
---
+++ Módulo 04/Projeto com Spring/introducao_spring/introducao_spring/src/test/java/br/com/dio/santander/introducao_spring/IntroducaoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.santander.introducao_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
